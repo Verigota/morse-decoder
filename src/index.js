be5c92c0f56ccba504798888713858de80eb1726
Tatiana <amora4ka@gmail.com>
2022-02-16 17:51:43 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = '';
+    const arrWords = expr.split('**********');
+    arrWords.forEach (word => {
+        for ( let i = 0; i < word.length; i += 10){
+            let letter = word.substring(i, i+10);
+            let ind = letter.indexOf('1');
+            letter = letter.slice(ind);
+            let symbol = '';
+            const dot = '.';
+            const tir = '-'
+            for (let j = 1; j < letter.length; j += 2){
+                if (letter[j] === '0') {
+                    symbol = symbol + dot;
+                } else {
+                    symbol = symbol + tir;
+                }
+            }
+
+            str = str.concat(MORSE_TABLE[symbol]);
+        }
+        str = str.concat(' ')
+    })
+    return str.slice(0, str.length-1)
 }
 
 module.exports = {
